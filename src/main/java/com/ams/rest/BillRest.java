1f1d8e25efdcc3a0598c27021d95092ae6cf40ad
Danil Melikyan <on.melikyan@yandex.ru>
2022-01-12 19:15:23 +0300

    add BillRest

---
+++ src/main/java/com/ams/rest/BillRest.java
@@ -0,0 +1,26 @@
+package com.ams.rest;
+
+import com.ams.service.BillService;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("bill")
+public class BillRest {
+
+    private final BillService billService;
+
+    public BillRest(BillService billService) {
+        this.billService = billService;
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_PDF_VALUE)
+    public @ResponseBody byte[] getReport(@PathVariable int id) throws IOException {
+        return billService.getBill(id);
+    }
+}
--- src/main/java/com/ams/service/workflow/delegate/GoodsReservationDelegate.java
+++ src/main/java/com/ams/service/workflow/delegate/GoodsReservationDelegate.java
@@ -2,6 +2,7 @@ package com.ams.service.workflow.delegate;
 
 import com.ams.security.AuthorityType;
 import com.ams.service.ApplicationService;
+import com.ams.service.BillService;
 import com.ams.service.LoadBalancer;
 import com.ams.service.WarehouseService;
 import com.ams.service.application.Application;
@@ -20,13 +21,16 @@ public class GoodsReservationDelegate implements JavaDelegate {
     private final ApplicationService applicationService;
     private final WarehouseService warehouseService;
     private final LoadBalancer loadBalancer;
+    private final BillService billService;
 
     public GoodsReservationDelegate(ApplicationService applicationService,
                                     WarehouseService warehouseService,
-                                    LoadBalancer loadBalancer) {
+                                    LoadBalancer loadBalancer,
+                                    BillService billService) {
         this.applicationService = applicationService;
         this.warehouseService = warehouseService;
         this.loadBalancer = loadBalancer;
+        this.billService = billService;
     }
 
     @Override
@@ -43,5 +47,7 @@ public class GoodsReservationDelegate implements JavaDelegate {
             execution.setVariable(WorkflowConstants.NEED_DELIVERY_KEY, false);
             execution.setVariable(WorkflowConstants.RESPONSIBLE_ASSISTANT_KEY, application.getManagerUsername());
         }
+
+        billService.saveBillAsPdf(id);
     }
 }
